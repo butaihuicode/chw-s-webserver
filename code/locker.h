156be7e12b9310afb38ca1fdb79e65ed52d2f150
butaihuicode <91518739+butaihuicode@users.noreply.github.com>
2023-02-23 10:55:41 +0800

    Update locker.h

--- code/locker.h
+++ code/locker.h
@@ -5,10 +5,108 @@
 #ifndef VSCODE_PROJECT_LOCKER_H
 #define VSCODE_PROJECT_LOCKER_H
 
-#ifndef LOCKER_H
-#define LOCKER_H
+#include <semaphore.h>
+#include <exception>
+#include <stdexcept>
+#include <iostream>
 
-#include <unistd.h>
+class Locker
+{
+public:
+    explicit Locker(int _pshared, unsigned int value)
+    {
+        _init(_pshared, value);
+    }
+    ~Locker()
+    {
+        _destroy();
+    }
+    Locker(const Locker &) = delete;
+    Locker(const Locker &&) = delete;
+    Locker &operator=(const Locker &) = delete;
+
+    bool wait()
+    {
+        int ret = 0;
+        try
+        {
+            int ret = sem_wait(&m_sem);
+            if (ret != 0)
+            {
+                throw std::runtime_error("call sem_wait() faild");
+            }
+        }
+        catch (const std::runtime_error &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+
+        return ret == 0;
+    }
+    bool post()
+    {
+        int ret = 0;
+        try
+        {
+            int ret = sem_post(&m_sem);
+            if (ret != 0)
+            {
+                throw std::runtime_error("call sem_post() faild");
+            }
+        }
+        catch (const std::runtime_error &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+
+        return ret == 0;
+    }
+
+private:
+    sem_t m_sem;
+    void _init(int _pshared, unsigned int value)
+    {
+        try
+        {
+            if (value < 0)
+            {
+                throw std::invalid_argument("The value cannot be less than 0");
+            }
+            int ret = sem_init(&m_sem, _pshared, value);
+            if (ret != 0)
+            {
+                throw std::runtime_error("call sem_init() faild");
+            }
+        }
+        catch (const std::invalid_argument &e)
+        {
+            std::cerr << e.what() << "\n";
+        }
+        catch (const std::runtime_error &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+    void _destroy()
+    {
+        try
+        {
+            int ret = sem_destroy(&m_sem);
+            if (ret != 0)
+            {
+                throw std::runtime_error("call sem_destroy() faild");
+            }
+        }
+        catch (const std::runtime_error &e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
+};
+
+
+
+/*#include <unistd.h>
 #include <stdlib.h>
 #include <mutex>
 #include <semaphore.h>
@@ -117,8 +215,7 @@ public:
     {
         return sem_trywait(&m_sem) == 0;
     }
-};
+}; */
 
-#endif
 
 #endif // VSCODE_PROJECT_LOCKER_H
